39f3f7c87dd36645fcc75f957a4c1056b98458eb
Anna-12-Mer <mammeramna@gmail.Com>
2020-12-24 11:21:31 +0100

    add routes

---
+++ techShop_ReactJs/backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const products = require('./data/products')
+
+const app = express(); 
+app.get('/', (req, res)=>{
+    res.send('API is running ...'); 
+});
+
+// Get Products 
+app.get('/api/products', (req, res)=>{
+    res.json(products)
+});
+// Get product by _id 
+app.get('/api/products/:id', (req, res)=>{
+    const product = products.find((p) => p._id === req.params._id)
+    res.json(product)
+});
+
+app.listen(5000,console.log('Server running on port 5000'))
